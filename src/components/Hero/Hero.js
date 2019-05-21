4ad3bafc416a180b8f5a99abd928945647d2cf91
zackcarlson <zackt.carlson@gmail.com>
2019-05-20 17:25:49 -0700

    Create updateRange functionality and pass down

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,13 +6,24 @@ import './Hero.css';
 export default class Hero extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      rangeVal: 0.05
+    };
+    this.updateRange = this.updateRange.bind(this);
   }
+
+  updateRange(val, e) {
+    this.setState({
+      rangeVal: val
+    });
+  } 
+
   render() {
+    const { rangeVal } = this.state;
     return (
       <div className='hero-container'>
         <Nav/>
-        <GetStarted/>
+        <GetStarted range={rangeVal} updateRange={this.updateRange}/>
       </div>
     );
   }
