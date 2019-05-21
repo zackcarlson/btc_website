700647e6ad55a85eaccca3255c51756bd9162aa1
zackcarlson <zackt.carlson@gmail.com>
2019-05-20 23:15:52 -0700

    Add a headerClass property to Header component

--- src/components/Hero/Header/Header.js
+++ src/components/Hero/Header/Header.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Header = (props) => (
   <span className='header-container'>
-    <header className='header-root'>{props.headerText}</header>
+    <header className={'header-root ' + props.headerClass || ''} >
+      {props.headerText}
+    </header>
   </span>
 );
 
