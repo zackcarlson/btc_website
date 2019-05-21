04629ce48d6acd54d80e006f029d43754882e589
zackcarlson <zackt.carlson@gmail.com>
2019-05-20 23:16:23 -0700

    Reorder navbar items

--- src/components/Hero/Nav/Nav.js
+++ src/components/Hero/Nav/Nav.js
@@ -3,7 +3,6 @@ import './Nav.css';
 
 const Nav = () => (
   <nav className='navbar'>
-    {/* <div className='navbar-brand'>Changelly</div> */}
     <ul className='navbar-menu'>
       <li className='navbar-item'>
         <span className='navbar-brand'>Changelly</span>
