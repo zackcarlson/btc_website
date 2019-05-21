10695626d8e20551fccfb2257d985439a0b6888a
zackcarlson <zackt.carlson@gmail.com>
2019-05-20 23:15:14 -0700

    Restyle GetStarted with bootstrap

--- src/components/Hero/GetStarted/GetStarted.js
+++ src/components/Hero/GetStarted/GetStarted.js
@@ -8,18 +8,26 @@ import './GetStarted.css';
 
 const GetStarted = (props) => (
   <div className='getstarted-container'>
-    <div className='getstarted-info-container'>
-      <Header headerText={'Buy Bitcoin with Credit Card'}/>
-      <Paragraph paragraphText={
-        `Catch mouse and gave it as a present chew the plant 
-        but scratch me there, elevator butt. Always ensure to lay down in 
-        such a manner that tail can lightly brush.`}
-        paragraphClass={'getStarted-p'}
-      />
-      <Button btnText={'Create Account'} btnClass={'getStarted-btn'}/>
-    </div>
-    <div className='getstarted-buy-container'>
-      <Buy />
+    <div className='container'>
+      <div className='row'>
+        <div className='col-sm-6'>
+          <div className='getstarted-info-container'>
+            <Header headerText={'Buy Bitcoin with Credit Card'}/>
+            <Paragraph paragraphText={
+              `Catch mouse and gave it as a present chew the plant 
+              but scratch me there, elevator butt. Always ensure to lay down in 
+              such a manner that tail can lightly brush.`}
+              paragraphClass={'getStarted-p'}
+            />
+            <Button btnText={'Create Account'} btnClass={'getStarted-btn'}/>
+          </div>
+        </div>
+        <div className='col-sm-6'>
+          <div className='getstarted-buy-container'>
+            <Buy />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
